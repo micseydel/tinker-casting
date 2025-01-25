da264c0ae9d93f7394ff5e5d1ae015256ce74659
Michael Seydel <micseydel@gmail.com>
2025-01-24 17:26:31 -0800

    [wyze] Added /wyze/plug and switched over to using it, also added prototype on/off detection for the note

--- scripts/wyze_wrapper/wyze_server.py
+++ scripts/wyze_wrapper/wyze_server.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import time
+from typing import Sequence
 
 from wyze_sdk import Client
+from wyze_sdk.api.devices import PlugsClient
 from wyze_sdk.errors import WyzeApiError
 from flask import Flask, request, jsonify
+from wyze_sdk.models.devices import Plug
 
 app = Flask(__name__)
 
@@ -16,15 +19,37 @@ def get_wyze_device_list():
     return jsonify(result), 200
 
 
+def to_dict_better(p: Plug):
+    d = p.to_dict()
+    
+    if p.is_online:
+        d["is_on"] = p.is_on
+
+    return d
+
+
+@app.route('/wyze/plug', methods=['GET'])
+def get_wyze_plug_list():
+    devices = [to_dict_better(e) for e in client.plugs.list()]
+    result = {"wyze_plug_list": devices}
+    return jsonify(result), 200
+
+
+###
+
+
 def get_environment_variable(name: str) -> str:
     value = os.environ.get(name)
     if value is None:
+        print_help()
         raise Exception(f"Required environment variable {name} not present")
 
     return value
 
+
 def print_help():
-    print(f"This script requires the port provided as a CLI param and the following environment variables: {{WYZE_EMAIL, WYZE_PASSWORD, WYZE_KEY_ID, WYZE_API_KEY}}")
+    print(f"This script requires the port provided as a CLI param and the following environment variables: "
+          f"{{WYZE_EMAIL, WYZE_PASSWORD, WYZE_KEY_ID, WYZE_API_KEY}}")
 
 
 if __name__ == "__main__":
@@ -36,7 +61,13 @@ if __name__ == "__main__":
     key_id = get_environment_variable("WYZE_KEY_ID")
     api_key = get_environment_variable("WYZE_API_KEY")
 
+    # FIXME global, lazy
     client = Client(email=email, password=password, key_id=key_id, api_key=api_key)
+
+    plugs: PlugsClient = client.plugs
+    t: Sequence[Plug] = plugs.list()
+    plug: Plug = t[0]
+    plug.to_dict()
 
     app.run(
             # host='0.0.0.0',  # listen on the network, not just localhost
--- src/main/scala/me/micseydel/actor/WyzeActor.scala
+++ src/main/scala/me/micseydel/actor/WyzeActor.scala
@@ -8,6 +8,8 @@ import me.micseydel.dsl.{SpiritRef, Tinker, TinkerColor, TinkerContext, Tinkerer
 import me.micseydel.dsl.Tinker.Ability
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import me.micseydel.actor.VaultPathAdapter.VaultPathUpdatedEvent
+import me.micseydel.vault.persistence.NoteRef
 
 import scala.annotation.unused
 import scala.concurrent.Future
@@ -16,16 +18,21 @@ import scala.util.{Failure, Success}
 object WyzeActor {
   sealed trait Message
 
-  case class ReceiveWyzeAPIResponse(response: WyzeAPIResponse) extends Message
+  private final case class ReceiveWyzeAPIResponse(response: WyzeAPIResponse) extends Message
+  private final case class ReceiveVaultPathUpdatedEvent(vaultPathUpdatedEvent: VaultPathUpdatedEvent) extends Message
 
   private val NoteName = "Wyze tinkering"
 
   def apply(wyzeUri: String)(implicit Tinker: Tinker): Ability[Message] = Tinkerer(TinkerColor.rgb(0, 255, 255), "🔌").setup { _ =>
-    Tinker.initializedWithNote(NoteName) { (context, noteRef) =>
+    Tinker.initializedWithNote(NoteName, "_actor_notes/wyze") { (context, noteRef) =>
+      implicit val c: TinkerContext[_] = context
+
       val request = HttpRequest(
         method = HttpMethods.GET,
-        uri = s"http://$wyzeUri/wyze/device"
+        uri = s"http://$wyzeUri/wyze/plug"
       )
+
+      context.system.actorNotesFolderWatcherActor !! ActorNotesFolderWatcherActor.Subscribe("wyze", context.messageAdapter(ReceiveVaultPathUpdatedEvent))
 
       import WyzeAPIJsonProtocol.wyzeAPIResponseResultJsonFormat
 
@@ -33,30 +40,107 @@ object WyzeActor {
       val devicesFetcher: SpiritRef[HttpFetchAndUnmarshall.Message[WyzeAPIResponse]] =
         context.castAnonymous(HttpFetchAndUnmarshall(request, context.messageAdapter(ReceiveWyzeAPIResponse), FailedWyzeAPIResponse))
 
-      Tinker.withMessages {
-        case ReceiveWyzeAPIResponse(response) =>
-          response match {
-            case FailedWyzeAPIResponse(exception) =>
-              context.actorContext.log.error("Failed to get device list", exception)
-              Tinker.steadily
-            case WyzeAPIResponseResult(wyzeDeviceList) =>
-              context.actorContext.log.info(s"Received device list, writing to $NoteName a total of ${wyzeDeviceList.size} devices")
+      initializing(noteRef)
+    }
+  }
 
-              noteRef.setMarkdown {
-                val devices = wyzeDeviceList.map {
-                  case OutdoorPlug(ip, mac, nickname, photosensitive_switch, push_switch, rssi, ssid) =>
-                    s"- $nickname: $mac"
-                }.mkString("\n") + "\n"
+  private def initializing(noteRef: NoteRef)(implicit Tinker: Tinker): Ability[Message] = Tinker.receive { (context, message) =>
+    message match {
+      case ReceiveVaultPathUpdatedEvent(event) =>
+        context.actorContext.log.info(s"Not yet initialized but received event $event")
+        Tinker.steadily
 
-                s"""- generated ${context.system.clock.now()}
-                   |# Devices
-                   |
-                   |$devices""".stripMargin
-              }
+      case ReceiveWyzeAPIResponse(response) =>
+        response match {
+          case FailedWyzeAPIResponse(exception) =>
+            context.actorContext.log.error("Failed to get device list", exception)
+            Tinker.steadily
+          case WyzeAPIResponseResult(wyzeDeviceList) =>
+            // FIXME: cache the is_on state, have an initiaization
+            context.actorContext.log.info(s"Received device list, writing to $NoteName a total of ${wyzeDeviceList.size} devices")
 
-              Tinker.steadily
-          }
-      }
+            noteRef.setMarkdown {
+              val devices = wyzeDeviceList.map {
+                case OutdoorPlug(ip, mac, nickname, maybe_is_on) =>
+                  maybe_is_on match {
+                    case Some(true) =>
+                      s"- [ ] $nickname: $mac"
+                    case Some(false) =>
+                      s"- [x] $nickname: $mac"
+                    case None =>
+                      s"- $nickname: $mac"
+                  }
+              }.mkString("\n") + "\n"
+
+              s"""- generated ${context.system.clock.now()}
+                 |# Devices
+                 |
+                 |$devices""".stripMargin
+            }
+
+            val isOnMap = wyzeDeviceList.flatMap {
+              case OutdoorPlug(ip, mac, nickname, maybe_is_on) =>
+                maybe_is_on.map(mac -> _)
+            }.toMap
+
+            initialized(isOnMap)(Tinker, noteRef)
+        }
+    }
+  }
+
+  private val Off = 'x'
+  private val On = ' '
+
+  private def initialized(isOnMap: Map[String, Boolean])(implicit Tinker: Tinker, noteRef: NoteRef): Ability[Message] = Tinker.receive { (context, message) =>
+    message match {
+      case ReceiveVaultPathUpdatedEvent(event) =>
+        event match {
+          case VaultPathAdapter.PathModifiedEvent(_) =>
+            context.actorContext.log.info("File updated, reading Markdown...")
+            noteRef.readMarkdown() match {
+              case Failure(exception) => throw exception
+              case Success(markdown) =>
+                val isOnMapFromDisk = markdown.split("\n")
+                  .filter(_.startsWith("- ["))
+                  .flatMap { line =>
+                    val maybeOnOff: Option[Boolean] = line(3) match {
+                      case Off => Some(false)
+                      case On => Some(true)
+                      case other =>
+                        context.actorContext.log.warn(s"Wierd line: $line")
+                        None
+                    }
+
+                    maybeOnOff.flatMap { onOff =>
+                      line.split(": ").toList match {
+                        case List(_, mac) =>
+                          Some(mac -> onOff)
+                        case _ =>
+                          context.actorContext.log.warn(s"Expected nickname:mac but got `$line``")
+                          None
+                      }
+                    }
+                  }.toMap
+
+                isOnMap.foreach { case (mac, cachedState) =>
+                  isOnMapFromDisk.get(mac) match {
+                    case Some(stateOnDisk) =>
+                      if (stateOnDisk != cachedState) {
+                        context.actorContext.log.info(s"FYI, mac $mac changed its on/off state from $cachedState to $stateOnDisk")
+                      }
+
+                    case None =>
+                  }
+                }
+            }
+
+          case VaultPathAdapter.PathCreatedEvent(_) | VaultPathAdapter.PathDeletedEvent(_) => ???
+        }
+        Tinker.steadily
+
+      case ReceiveWyzeAPIResponse(response) =>
+        context.actorContext.log.warn(s"Did not expect a Wyze response after initialization: $response")
+        Tinker.steadily
     }
   }
 
@@ -66,16 +150,16 @@ object WyzeActor {
 
   case class FailedWyzeAPIResponse(exception: Throwable) extends WyzeAPIResponse
 
-  case class WyzeAPIResponseResult(wyze_device_list: List[WyzeDevice]) extends WyzeAPIResponse
+  case class WyzeAPIResponseResult(wyze_plug_list: List[WyzeDevice]) extends WyzeAPIResponse
 
   // api
 
   sealed trait WyzeDevice
 
-  case class OutdoorPlug(ip: String, mac: String, nickname: String, photosensitive_switch: Boolean, push_switch: Int, rssi: String, ssid: String) extends WyzeDevice
+  case class OutdoorPlug(ip: String, mac: String, nickname: String, is_on: Option[Boolean]) extends WyzeDevice
 
   object WyzeAPIJsonProtocol extends DefaultJsonProtocol {
-    implicit val outdoorPlugJsonFormat: RootJsonFormat[OutdoorPlug] = jsonFormat7(OutdoorPlug)
+    implicit val outdoorPlugJsonFormat: RootJsonFormat[OutdoorPlug] = jsonFormat4(OutdoorPlug)
 
     implicit object WyzeDeviceJsonFormat extends RootJsonFormat[WyzeDevice] {
       def write(m: WyzeDevice): JsValue = {
@@ -87,7 +171,7 @@ object WyzeActor {
 
       def read(value: JsValue): WyzeDevice = {
         value.asJsObject.getFields("type") match {
-          case Seq(JsString("OutdoorPlug")) => value.convertTo[OutdoorPlug]
+          case Seq(JsString("OutdoorPlug" | "Plug")) => value.convertTo[OutdoorPlug]
           case other => throw DeserializationException(s"""Unknown type $other; expected Seq(JsString("OutdoorPlug"))""")
         }
       }
