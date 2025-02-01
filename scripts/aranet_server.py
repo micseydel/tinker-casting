4f5e989485510e6c3e6a46995a4f058142ea97ca
Michael Seydel <micseydel@gmail.com>
2025-01-31 21:13:29 -0800

    Followup from prior commit, overwrote an old file instead of keeping duplicate code

--- scripts/aranet_server.py
+++ scripts/aranet_server.py
@@ -1,49 +1,52 @@
 import sys
 import asyncio
 
+from bleak import BleakScanner
 from flask import Flask, request, jsonify
 import time
-from claranet4.lib import *  # FIXME: discover_ara4s and what else?
+from claranet4.lib import discover, Device, DeviceError, request_measurements, Reading
 
 app = Flask(__name__)
 
+# FIXME: copy paste from the library, changed for async
+async def discover_ara4s(substring: str = "Aranet4") -> list[Device]:
+    devices = await discover()
+    ara4_devices = [d for d in devices if substring in d.name]
+    return ara4_devices
 
 @app.route('/ara4s', methods=['GET'])
-async def ara4s_exp():
-    result = await get_aras_result(15)
+async def get_ara4s():
+    aras = await discover_ara4s()
+
+    addresses = [ara.address for ara in aras]
+    result = await get_aras_result(addresses, 15)
+
     return jsonify(result)
 
 
-async def read_ara(uuid, timeout_seconds):
+async def read_ara(address, timeout_seconds):
     try:
-        result = await asyncio.wait_for(claranet_read_async(uuid), timeout_seconds)
+        result = await asyncio.wait_for(claranet_read_async(address), timeout_seconds)
         return result
     except asyncio.TimeoutError:
-        print(f"Read timed out for UUID {uuid} after {timeout_seconds} seconds")
+        print(f"Read timed out for address {address} after {timeout_seconds} seconds")
     except Exception as e:
-        print(f"An error occurred while reading UUID {uuid}: {e}")
+        print(f"An error occurred while reading address {address}: {e}")
     return None
 
 
-async def get_aras_result(timeout_seconds):
+async def get_aras_result(addresses, timeout_seconds):
     start = time.perf_counter()
 
-    # ara_1, ara_2 = await asyncio.gather(
-    #     read_ara(ARA_1_UUID, timeout_seconds),
-    #     read_ara(ARA_2_UUID, timeout_seconds)
-    # )
-
-    result = await asyncio.wait_for(read_ara(araUuid, timeout_seconds), timeout_seconds)
+    results = await asyncio.gather(
+        *[read_ara(address, timeout_seconds) for address in addresses]
+    )
 
     elapsed = time.perf_counter() - start
     print(f"Aras fetching took {elapsed:.1f}s")
 
-    # aras = {key: value for key, value in zip(["1", "2"], [ara_1, ara_2]) if value is not None}
-
-    aras = {araId: result} if result is not None else {}
-
     return {
-        "aras": aras,
+        "aras": results,
         "meta": {
             "elapsed": elapsed,
             "captureTime": int(time.time())
@@ -54,11 +57,11 @@ async def get_aras_result(timeout_seconds):
 
 # FIXME: these two functions were copy-pasted from the library with modifications for async
 
-async def claranet_read_async(address: str = "", timeout: int = 5) -> Reading:
+async def claranet_read_async(address: str = "", timeout: int = 5) -> dict: # -> Reading:
     if address:
         device = await claranet_find_device_async(address)
     else:
-        ara4_devices = discover_ara4s(timeout=timeout)
+        ara4_devices = discover_ara4s() # discover_ara4s(timeout=timeout)
         if not ara4_devices:
             raise DeviceError("No Aranet4 devices discovered")
         else:
@@ -82,16 +85,15 @@ async def claranet_find_device_async(address) -> Device:
 
 if __name__ == '__main__':
     try:
-        # FIXME: globals
-        _, port, araId, araUuid = sys.argv
+        _, port = sys.argv
         port = int(port)
     except ValueError as e:
-        print("Expected: a port (int), aranet id (string), aranet uuid (string)")
+        print("Expected: a port (int)")
         raise e
     else:
         app.run(
             # host='0.0.0.0',  # listen on the network, not just localhost
-            port=int(port),
+            port=port,
             debug=True,
             # I haven't tested hot reloading for whisper models
             use_reloader=False
