2361b15f03f7d0776deaf68254c47f60933bc55b
Michael Seydel <micseydel@gmail.com>
2026-02-05 08:56:58 -0800

    HumidityWatcherActor now includes time labels on the chart's x-axis

--- src/main/scala/me/micseydel/actor/HumidityWatcherActor.scala
+++ src/main/scala/me/micseydel/actor/HumidityWatcherActor.scala
@@ -3,11 +3,12 @@ package me.micseydel.actor
 import me.micseydel.NoOp
 import me.micseydel.actor.perimeter.AranetActor
 import me.micseydel.actor.perimeter.AranetActor.{Aranet, AranetJsonProtocol, AranetResults}
+import me.micseydel.dsl.*
 import me.micseydel.dsl.Tinker.Ability
 import me.micseydel.dsl.tinkerer.NoteMakingTinkerer
-import me.micseydel.dsl.*
 import me.micseydel.prototyping.ObsidianCharts
 import me.micseydel.prototyping.ObsidianCharts.DoubleSeries
+import me.micseydel.util.TimeUtil
 
 import scala.util.{Failure, Success}
 
@@ -64,11 +65,15 @@ object HumidityWatcherActor {
         "No measurements\n"
       case _ =>
         val measurements: List[Double] = items.sortBy(_.meta.captureTime).map(_.averageHumidity)
-        val averageChart = ObsidianCharts.chart(List.fill(measurements.size)(""), List(DoubleSeries("average", measurements)))
+        val averageChart = ObsidianCharts.chart(sparseLabelsExperiment(items), List(DoubleSeries("average", measurements)))
 
-        // FIXME: hack
-        val justBedroomHumidity = items.flatMap(_.aras).filter(_.name == "Aranet4 29686").map(_.humidity)
-        val justBedroomChart = ObsidianCharts.chart(List.fill(justBedroomHumidity.size)(""), List(DoubleSeries("bedroom", justBedroomHumidity)))
+        // FIXME: hack - how can I configure this from Markdown/Frontmatter instead?
+        val justBedroomMeasurements: List[AranetResults] = items.filter(_.aras.exists(_.name == "Aranet4 29686"))
+        val justBedroomHumidity = justBedroomMeasurements.flatMap(_.aras.filter(_.name == "Aranet4 29686")).map(_.humidity)
+        val justBedroomChart = ObsidianCharts.chart(
+          sparseLabelsExperiment(justBedroomMeasurements),
+          List(DoubleSeries("bedroom", justBedroomHumidity))
+        )
 
         val latest: AranetResults = items.last
 
@@ -85,5 +90,10 @@ object HumidityWatcherActor {
            |$averageChart
            |""".stripMargin
     }
+  }
+
+  private def sparseLabelsExperiment(measurements: List[AranetResults]): List[String] = {
+    // FIXME it turns out the Charts plugin is a little smart about this, so punting the effort
+    measurements.map(_.meta.captureTime).map(_.format(TimeUtil.WithinDay24HourDateTimeFormatter))
   }
 }
