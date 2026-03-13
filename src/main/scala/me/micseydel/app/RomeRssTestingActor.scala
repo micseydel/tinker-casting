29389c66738a3e6c64c7d70395b8f6c66b4c5e8e
Michael Seydel <micseydel@gmail.com>
2026-03-12 21:42:05 -0700

    RomeRssTestingActor now pulls uri from yaml config, triggered via button

--- src/main/scala/me/micseydel/app/RomeRssTestingActor.scala
+++ src/main/scala/me/micseydel/app/RomeRssTestingActor.scala
@@ -4,11 +4,16 @@ import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse, ResponseEntity}
 import akka.http.scaladsl.unmarshalling.Unmarshal
+import cats.data.ValidatedNel
 import com.rometools.rome.feed.synd.{SyndContent, SyndFeed}
 import com.rometools.rome.io.SyndFeedInput
+import me.micseydel.{Common, NoOp}
+import me.micseydel.actor.FolderWatcherActor.Ping
 import me.micseydel.dsl.Tinker.Ability
-import me.micseydel.dsl.tinkerer.NoteMakingTinkerer
+import me.micseydel.dsl.tinkerer.{AttentiveNoteMakingTinkerer, NoteMakingTinkerer}
 import me.micseydel.dsl.{Tinker, TinkerColor}
+import me.micseydel.vault.Note
+import me.micseydel.vault.persistence.NoteRef
 
 import java.io.StringReader
 import scala.collection.mutable
@@ -19,20 +24,38 @@ import scala.util.{Failure, Success, Try}
 object RomeRssTestingActor {
   sealed trait Message
 
+  private case class ReceiveNotePing(ping: Ping) extends Message
+
   private case class ReceiveHttpResult(httpResponse: Try[HttpResponse]) extends Message
 
   private case class ReceiveUnmarshalling(raw: Try[String]) extends Message
 
-  def apply()(implicit Tinker: Tinker): Ability[Message] = NoteMakingTinkerer("RomeRssTestingActor", TinkerColor.random(), "🙉") { (context, noteRef) =>
+  def apply()(implicit Tinker: Tinker): Ability[Message] = AttentiveNoteMakingTinkerer[Message, ReceiveNotePing]("RomeRssTestingActor", TinkerColor.random(), "🙉", ReceiveNotePing) { (context, noteRef) =>
     implicit val s: ActorSystem[?] = context.system.actorSystem
 
     // "https://stackoverflow.com/feeds/tag?tagnames=rome"
-    val uri = "https://www.reddit.com/r/openclaw.rss"
-    context.pipeToSelf(Http().singleRequest(
-      HttpRequest(method = HttpMethods.GET, uri = uri)
-    ))(ReceiveHttpResult)
+    //    val uri = "https://www.reddit.com/r/openclaw.rss"
+//    val uri = "https://feeds.simplecast.com/RqijYekb"
+    noteRef.setMarkdown(s"""- [ ] *fetch now*""") match {
+      case Failure(exception) => throw exception
+      case Success(NoOp) =>
+    }
 
     Tinker.receiveMessage {
+      case ReceiveNotePing(NoOp) =>
+        noteRef.checkBoxAndMaybeUri() match {
+          case Failure(exception) => context.actorContext.log.warn("Checking note failed", exception)
+          case Success((true, Some(uri))) =>
+            context.actorContext.log.info("Fetching feed...")
+            context.pipeToSelf(Http().singleRequest(
+              HttpRequest(method = HttpMethods.GET, uri = uri)
+            ))(ReceiveHttpResult)
+          case Success((boxChecked, maybeUri)) =>
+            context.actorContext.log.debug(s"(boxChecked, maybeUri) = ($boxChecked, $maybeUri)")
+        }
+
+        Tinker.steadily
+
       case ReceiveHttpResult(httpResult) =>
         httpResult match {
           case Failure(exception) => throw exception
@@ -84,7 +107,8 @@ object RomeRssTestingActor {
             }.mkString("\n")
 
             noteRef.setMarkdown(
-              s"""- generated ${context.system.clock.now()}
+              s"""- [ ] *fetch now*
+                 |- generated ${context.system.clock.now()}
                  |- ${feed.getEntries.size()} entries
                  |
                  |# Entries
@@ -113,5 +137,30 @@ object RomeRssTestingActor {
     val input: SyndFeedInput = new SyndFeedInput()
     val feed: SyndFeed = input.build(new StringReader(raw))
     (raw, feed)
+  }
+
+  private implicit class RichNoteRef(val noteRef: NoteRef) extends AnyVal {
+    def checkBoxAndMaybeUri(): Try[(Boolean, Option[String])] = {
+      noteRef.readNote().flatMap { note =>
+        val checkBoxIsChecked: Try[Boolean] = if (note.markdown.startsWith("- [")) {
+          Try(note.markdown.charAt(3) == 'x')
+        } else {
+          Failure(new RuntimeException(s"Expected markdown to start with \"- [\" but instead it started with: ${note.markdown.take(4)}"))
+        }
+
+        val maybeConfig: Try[Option[String]] = {
+          val maybeUri: Try[Option[Any]] = note.yamlFrontMatter.map(_.get("uri"))
+
+          maybeUri.map {
+            case Some(value: String) => Some(value)
+            case None => None
+            case Some(_) => None // FIXME better non-happy path
+          }
+        }
+
+        checkBoxIsChecked
+          .flatMap(checkBoxChecked => maybeConfig.map(checkBoxChecked -> _))
+      }
+    }
   }
 }
