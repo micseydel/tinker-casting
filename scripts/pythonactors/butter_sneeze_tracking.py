e07400fa9dd490e45f0c0b74a20aacc90688bd5c
Michael Seydel <micseydel@gmail.com>
2026-04-25 16:52:05 -0700

    [butter_sneeze_tracking.py] proof of concept for using the note from Python (no more stdout after startup until Ctrl+C, yay)

--- scripts/pythonactors/butter_sneeze_tracking.py
+++ scripts/pythonactors/butter_sneeze_tracking.py
@@ -32,11 +32,11 @@ def print_with_time(s, *args, **kwargs) -> None:
 
 
 class PythonActor:
-    def __init__(self, vault_root):
-        self.path = os.path.join(vault_root, NoteName)
+    def __init__(self, document):
+        self.document = document
 
     def connect_mqtt(self, client):
-        self.mqtt_client = client # will need this for ack'ing transcriptions, voting, etc
+        self.mqtt_client = client
 
     def on_message(self, client, userdata, msg):
         incoming_topic = msg.topic
@@ -54,23 +54,27 @@ class PythonActor:
         if incoming_topic == ReceivingTopicGossiper:
             lowered_text = incoming_data["capture"]["whisperResult"]["whisperResultContent"]["text"].lower()
             if "sneez" in lowered_text:
-                
-                captureTime = incoming_data["capture"]["captureTime"]
-
-                print_with_time(f"Noticed a sneeze!", lowered_text)
-                msg = json.dumps({
-                    "noteId": incoming_data["noteId"],
+                self.document.appendToInbox(f"- Noticed a sneeze! {lowered_text}\n")
+                self.mqtt_client.publish("[[Chronicler]]", json.dumps({
+                    "noteId": incoming_data["capture"]["captureTime"]["noteId"],
                     "noteCreationDate": captureTime[:10],
                     "timeOfAck": datetime.datetime.now().astimezone().isoformat(),
                     "details": "sneez detected ([[Butter Sneeze Tracking|ref]])",
                     "setNoteState": "AutomaticallyIntegrated",
-                })
-                
-                self.mqtt_client.publish("[[Chronicler]]", msg)
+                }))
             else:
-                print_with_time(f"Ignoring:", lowered_text)
+                logging.info("Ignoring: %s", lowered_text)
         elif incoming_topic == ReceivingTopicLitterSiftings:
-            print_with_time(f"Received litter report for day {incoming_data['forDay']}, {len(incoming_data['report']['dataPoints'])} events")
+            self.document.appendToInbox(f"- Received litter report for day {incoming_data['forDay']}, {len(incoming_data['report']['datapoints'])} events\n")
+
+class Document:
+    def __init__(self, fh):
+        self.fh = fh
+
+    def appendToInbox(self, line):
+        self.fh.read() # lazy-coded seek to the end
+        self.fh.write(line)
+        self.fh.flush()
 
 
 def run():
@@ -86,30 +90,30 @@ def run():
     client_num = random.randint(0, 100)
     client_id = f'{NoteName}-{client_num}'
 
-    actor = PythonActor(vault_root)
-    
-    print_with_time(f"pid {os.getpid()}, client {client_id} connecting now...")
+    with open(os.path.join(vault_root, f"{NoteName}.md"), 'r+') as fh:
+        document = Document(fh)
+        actor = PythonActor(document)
+        
+        print_with_time(f"pid {os.getpid()}, client {client_id} connecting now...")
 
-    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2, client_id)
-    actor.connect_mqtt(client)
-    client.username_pw_set(username, password)
-    subscription_topics = [ReceivingTopicGossiper, ReceivingTopicLitterSiftings]
-    def on_connect(client, userdata, flags, reason_code, properties):
-        client.on_message = actor.on_message
-        print_with_time(f"Subscribing to {subscription_topics}")
-        for topic in subscription_topics:
-            client.subscribe(topic)
+        client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2, client_id)
+        actor.connect_mqtt(client)
+        client.username_pw_set(username, password)
+        subscription_topics = [ReceivingTopicGossiper, ReceivingTopicLitterSiftings]
+        def on_connect(client, userdata, flags, reason_code, properties):
+            client.on_message = actor.on_message
+            print_with_time(f"Subscribing to {subscription_topics}")
+            for topic in subscription_topics:
+                client.subscribe(topic)
 
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    
+        client.on_connect = on_connect
+        client.connect(broker, port)
+        
 
-    try:
-        client.loop_forever()
-    except KeyboardInterrupt:
-        print_with_time("(KeyboardInterrupt) Done; any output like 'There appear to be 1 leaked semaphore objects to clean up at shutdown' can be ignored")
-
-    worker.terminate()
+        try:
+            client.loop_forever()
+        except KeyboardInterrupt:
+            print_with_time("(KeyboardInterrupt) Done")
 
 
 if __name__ == '__main__':
