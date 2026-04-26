d937f9b1894c0adf69ee61bc64773bae9d344256
Michael Seydel <micseydel@gmail.com>
2026-04-25 18:56:22 -0700

    [butter_sneeze_tracking.py] initial chart (sifting count per day)

--- scripts/pythonactors/butter_sneeze_tracking.py
+++ scripts/pythonactors/butter_sneeze_tracking.py
@@ -51,30 +51,83 @@ class PythonActor:
             logging.exception("Decoding or parsing JSON failed ({%s})", e)
             return
 
-        if incoming_topic == ReceivingTopicGossiper:
-            lowered_text = incoming_data["capture"]["whisperResult"]["whisperResultContent"]["text"].lower()
-            if "sneez" in lowered_text:
-                self.document.appendToInbox(f"- Noticed a sneeze! {lowered_text}\n")
-                self.mqtt_client.publish("[[Chronicler]]", json.dumps({
-                    "noteId": incoming_data["capture"]["captureTime"]["noteId"],
-                    "noteCreationDate": captureTime[:10],
-                    "timeOfAck": datetime.datetime.now().astimezone().isoformat(),
-                    "details": "sneez detected ([[Butter Sneeze Tracking|ref]])",
-                    "setNoteState": "AutomaticallyIntegrated",
-                }))
-            else:
-                logging.info("Ignoring: %s", lowered_text)
-        elif incoming_topic == ReceivingTopicLitterSiftings:
-            self.document.appendToInbox(f"- Received litter report for day {incoming_data['forDay']}, {len(incoming_data['report']['datapoints'])} events\n")
+        try:
+            if incoming_topic == ReceivingTopicGossiper:
+                lowered_text = incoming_data["capture"]["whisperResult"]["whisperResultContent"]["text"].lower()
+                if "sneez" in lowered_text:
+                    self.document.appendToInbox(f"- Noticed a sneeze! {lowered_text}\n")
+                    self.mqtt_client.publish("[[Chronicler]]", json.dumps({
+                        "noteId": incoming_data["capture"]["captureTime"]["noteId"],
+                        "noteCreationDate": captureTime[:10],
+                        "timeOfAck": datetime.datetime.now().astimezone().isoformat(),
+                        "details": "sneez detected ([[Butter Sneeze Tracking|ref]])",
+                        "setNoteState": "AutomaticallyIntegrated",
+                    }))
+                else:
+                    logging.info("Ignoring: %s", lowered_text)
+            elif incoming_topic == ReceivingTopicLitterSiftings:
+                self.document.add_sifting(incoming_data['forDay'], incoming_data['report'])
+                self.document.appendToInbox(f"- Received litter report for day {incoming_data['forDay']}, {len(incoming_data['report']['datapoints'])} events\n")
+        except Exception as e:
+            logging.exception("Something went wrong ({%s})", e)
 
 class Document:
-    def __init__(self, fh):
-        self.fh = fh
+    def __init__(self, markdownf, jsonf):
+        self.markdownf = markdownf
+        self.jsonf = jsonf
+        self.siftings = {}
+        try:
+            self.siftings = json.load(jsonf)
+        except Exception as e:
+            logging.exception(f"failed to load siftings from disk ({e})")
+
+    def add_sifting(self, for_day, sifting):
+        if self.siftings.get(for_day) == sifting:
+            return  # existing, no need for change
+
+        self.siftings[for_day] = sifting
+        self.update_chart()
+
+        self.jsonf.truncate(0)
+        json.dump(self.siftings, self.jsonf, indent=4)
+        self.jsonf.flush() #FIXME delete?
+
+    def update_chart(self):
+        tuples = [(for_day, len(sifting["datapoints"])) for (for_day, sifting) in self.siftings.items()]
+        sifting_counts_by_day = sorted(tuples, key=lambda x: datetime.datetime.fromisoformat(x[0]))
+        labels = [x[0] for x in sifting_counts_by_day]
+        data = [x[1] for x in sifting_counts_by_day]
+
+        chart = {
+            "type": "line",
+            "labels": labels,
+            "series": [
+                {
+                    "title": "Siftings in day",
+                    "data": data,
+                },
+            ],
+        }
+
+        self.markdownf.seek(0)
+        new_lines = []
+        for line in self.markdownf:
+            new_lines.append(line)
+            if line == "```chart\n":
+                break
+        next(self.markdownf)  # discard the old chart; naive, assumes one line of json (rather than yaml)
+        new_lines.append(json.dumps(chart))
+        new_lines.append("\n")
+        new_lines.extend(self.markdownf)
+
+        self.markdownf.truncate(0)
+        self.markdownf.write("".join(new_lines))
+        self.markdownf.flush()
 
     def appendToInbox(self, line):
-        self.fh.read() # lazy-coded seek to the end
-        self.fh.write(line)
-        self.fh.flush()
+        self.markdownf.read() # lazy-coded seek to the end
+        self.markdownf.write(line)
+        self.markdownf.flush()
 
 
 def run():
@@ -90,8 +143,11 @@ def run():
     client_num = random.randint(0, 100)
     client_id = f'{NoteName}-{client_num}'
 
-    with open(os.path.join(vault_root, f"{NoteName}.md"), 'r+') as fh:
-        document = Document(fh)
+    markdown_path = os.path.join(vault_root, f"{NoteName}.md")
+    json_path = os.path.join(vault_root, "json", f"{NoteName.lower().replace(' ', '_')}.json")
+    with open(markdown_path, 'r+') as markdownf, open(json_path, 'r+') as jsonf:
+        document = Document(markdownf, jsonf)
+        document.update_chart()
         actor = PythonActor(document)
         
         print_with_time(f"pid {os.getpid()}, client {client_id} connecting now...")
